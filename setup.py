7c7bc9964c2eb92560e623f3bea4053a0c91fe98
Aliaksei Urbanski <aliaksei@inspectorio.com>
2018-09-19 11:50:56 +0300

    Add missing data into setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@ from setuptools import find_packages, setup
 
 setup(
     python_requires='>=2.7',
+    author='Aliaksei Urbanski',
+    author_email='aliaksei@inspectorio.com',
+    url='https://gitlab.inspectorio.com/saas/libs/inspectorio-tracing',
     name='intracing',
     version='1.0.0',
     description='Inspectorio Tracing Helper',
