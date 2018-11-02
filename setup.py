9f3aa0cb53eb953b2799317b063f19cfca01237e
Aliaksei Urbanski <aliaksei@inspectorio.com>
2018-11-02 16:06:01 +0300

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,30 @@
+import os
+import re
+
 from setuptools import find_packages, setup
 
-import intracing
-
 OPENTRACING_INSTRUMENTATION_VERSION = '2.5.0.dev0'
+PACKAGE_NAME = 'intracing'
+
+
+def get_version():
+    init_path = os.path.join(
+        os.path.abspath(os.path.dirname(__file__)), PACKAGE_NAME, '__init__.py'
+    )
+    with open(init_path) as init_file:
+        return re.search(
+            r"^__version__ = '(?P<version>.*?)'.*$",
+            init_file.read(), re.MULTILINE
+        ).group('version')
+
 
 setup(
     python_requires='>=2.7',
     author='Aliaksei Urbanski',
     author_email='aliaksei@inspectorio.com',
     url='https://gitlab.inspectorio.com/saas/libs/inspectorio-tracing',
-    name='intracing',
-    version=intracing.__version__,
+    name=PACKAGE_NAME,
+    version=get_version(),
     description='Inspectorio Tracing Helper',
     install_requires=[
         'flask',
